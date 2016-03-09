b6e18c58b2377850d347c74ce8152bedd2352ccc
Viktor (Icon) VAD <vad.viktor@gmail.com>
2016-03-09 11:38:10 +0000

    update to ubuntu 14.04.4; expose docker API on TCP; remove vmware build

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -11,7 +11,7 @@ sudo apt-cache policy docker-engine
 sudo apt-get install -y linux-image-extra-$(uname -r) docker-engine apparmor cgroup-lite
 
 # enable Google DNS servers and lock in the default bridge ip
-sed -i 's*#DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"*DOCKER_OPTS="--bip=172.17.42.1/16 --dns 8.8.8.8 --dns 8.8.4.4"*g' /etc/default/docker
+sed -i 's*#DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"*DOCKER_OPTS="--bip=172.17.42.1/16 --dns 8.8.8.8 --dns 8.8.4.4 -H unix:///var/run/docker.sock -H tcp://0.0.0.0:4242"*g' /etc/default/docker
 
 # add docker group and add vagrant to it
 sudo groupadd docker
