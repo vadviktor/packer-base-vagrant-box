6406ff0a980493f31ad2a0931bafb8298bcebb6c
Viktor (Icon) VAD <vad.viktor@gmail.com>
2016-05-06 12:56:36 +0100

    clean vmware up

--- scripts/vmware.sh
+++
@@ -1,9 +0,0 @@
-apt-get -y install open-vm-tools
-
-#cd /tmp
-#mkdir -p /mnt/cdrom
-#mount -o loop ~/linux.iso /mnt/cdrom
-#tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-#/tmp/vmware-tools-distrib/vmware-install.pl -d
-#rm -f /home/vagrant/linux.iso
-#umount /mnt/cdrom
